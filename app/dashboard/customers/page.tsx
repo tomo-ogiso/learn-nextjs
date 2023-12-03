e459dc972810a3eabb3b09d4068c43b4e64fb3ae
tomo-ogiso <tomo.ogiso@gmail.com>
2023-12-03 14:15:52 +0900

    initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,4 @@
+export default function Page() {
+  console.log('aaaaaaaaa');
+  return <p>Customers Page</p>;
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inder, Lusitana } from 'next/font/google';
+
+export const inter = Inder({ subsets: ['latin'], weight: '400' });
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
